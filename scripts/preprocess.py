99c31cbd7e93a8e069cc355542a91805443d1ead
xblam <benlam05032004@gmail.com>
2025-07-10 01:01:50 +0700

    evaluated baseline model performance and error

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -9,16 +9,16 @@ from sklearn.model_selection import train_test_split
 def load_raw_data():
     return pd.read_csv("data/raw/train.csv")
 
-def add_features(df):
+def add_features(df: pd.DataFrame):
     return df
 
 # this is to preprocess the data for xgboost and the like, since trees handle data a little bit differently
-def preprocess_for_tree(df):
+def preprocess_for_tree(df: pd.DataFrame):
     # this will be empty since the data is already numeric
     return df
 
 # this is to further preprocess the data for standard methods, since I will need to one-hot encode some categories, since they have relatively arbitrary ratings
-def encode_features(df):
+def encode_features(df: pd.DataFrame):
     # for now i will also leave this blank
     return df
 
